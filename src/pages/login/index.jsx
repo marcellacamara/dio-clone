30cc16a502817232b94b3b7184f2f164203acfc4
marcella <marcella.camara@sagatechbrasil.com.br>
2022-12-20 02:23:24 -0300

    axios e fake api instalados

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -6,6 +6,7 @@ import * as yup from "yup";
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
+import { api } from '../../services/api';
 
 import { Column, Container, CriarText, EsqueciText, Row, SubtitleLogin, Title, TitleLogin, Wrapper } from './styles';
 
@@ -18,14 +19,25 @@ const Login = () => {
 
 	const navigate = useNavigate();
 
-	const { control, handleSubmit, formState: { errors, isValid } } = useForm({
+	const { control, handleSubmit, formState: { errors } } = useForm({
 		resolver: yupResolver(schema),
 		mode: 'onChange',
 	});
 
-	console.log(isValid, errors);
-
-	const onSubmit = data => console.log(data);
+	const onSubmit = async formData => {
+		try {
+			const { data } = await api.get(`
+				users?email=${formData.email}&senha=${formData.password}
+			`);
+			if (data.length === 1) {
+				navigate('/feed')
+			} else {
+				alert('Email ou senha inválido!')
+			}
+		} catch (error) {
+			alert('Houve um erro, tente novamente.')
+		}
+	};
 
 	return (<>
 		<Header />
